d8e5fd5944b1a1b7dbd35a51f47b8a0c28b92ccd
EveryWell <aneverywell@gmail.com>
2016-08-26 09:02:18 +0200

    fix

--- src/Essentials/OAuth/AuthServiceProvider.php
+++ src/Essentials/OAuth/AuthServiceProvider.php
@@ -16,7 +16,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         Auth::extend('oauth', function($app, $name, array $config) {
-
             return new OAuthGuard(Auth::createUserProvider($config['provider']), $this->app['request']);
         });
     }
